0c0ba52bff9d3fee591105c041be9bae657ed32d
Margaret Ko <margaretko@Margarets-MacBook-Air.local>
2025-12-19 14:37:13 +0200

    [~] returned logic for yesterday

--- src/main/make_wb_pay_daily.py
+++ src/main/make_wb_pay_daily.py
@@ -146,10 +146,10 @@ def process_daily_report(target_date):
 
 if __name__ == "__main__":
     # for yesterday
-    # yesterday = (datetime.now() - timedelta(days=1)).date()
-    # process_daily_report(yesterday)
+    yesterday = (datetime.now() - timedelta(days=1)).date()
+    process_daily_report(yesterday)
 
-    # for a time period
-    for day in range(18, 0, -1):
-        target_date = date(year = 2025, month = 12, day = day)
-        process_daily_report(target_date)
+    # # for a time period
+    # for day in range(18, 0, -1):
+    #     target_date = date(year = 2025, month = 12, day = day)
+    #     process_daily_report(target_date)
